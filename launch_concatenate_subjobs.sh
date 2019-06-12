8c6743a4316ad21adb249ef5ee51d7bbc1d345b1
Derek Shore <des2037@med.cornell.edu>
2019-06-12 18:13:40 -0400

    fixes default value

--- launch_concatenate_subjobs.sh
+++ launch_concatenate_subjobs.sh
@@ -5,7 +5,7 @@
 #BSUB -nnodes 1                     # this should equal: number_of_trajs/6 
 #BSUB -J test_concatenate_subjobs   # job name
 
-swarm_number=1
+swarm_number=0
 number_of_trajs_per_swarm=18
 catdcd="/gpfs/alpine/proj-shared/bip180/vmd/vmd_library/plugins/OPENPOWER/bin/catdcd5.1/catdcd"
 structure_file='dat_phase2b4.coor' # must be in ./common
