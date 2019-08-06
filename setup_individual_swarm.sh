2a8654fec917891c828283a56ebde609c26bf37b
Derek Shore <des2037@med.cornell.edu>
2019-08-06 16:15:18 -0400

    adds multi-system setup support

--- setup_individual_swarm.sh
+++ setup_individual_swarm.sh
@@ -6,14 +6,11 @@
 # This script sets up the directory/file structure, for common files, 
 # for an individual swarm. Here, a swarm is defined as a set of MD simulations
 # that will have different starting structures after the first swarm is run.
-#
-# This script does NOT provide the starting structure--this is swarm-dependent, and is 
-# controlled by other scripts.
 
 # Use: ./setup_individual.sh 
 
 swarm_number=0
-number_of_trajs_per_swarm=18
+number_of_trajs_per_swarm=24
 
 # do not edit below this line
 
@@ -26,20 +23,30 @@ mkdir -p $swarm_path
 # we start with host_number 1 because 0 is the launch node
 host_number=1
 
+# starting with input directory '0000'
+directoryNumber=0
+directoryNumberPadded=`printf %04d $directoryNumber`
+currentNumberOfReplicas=$(cat ./inputs/${directoryNumberPadded}/numberOfReplicas.txt)
+
+
 for (( traj_number=0; traj_number<$number_of_trajs_per_swarm; traj_number++ ))
 do
   traj_number_padded=`printf %04d $traj_number`
   traj_path=$swarm_path/swarm${swarm_number_padded}_traj$traj_number_padded
 
   mkdir $traj_path
+  cp ./common/readInputFiles.py $traj_path/.
+  cp ./common/run_python.sh $traj_path/.
+  cp ./inputs/${directoryNumberPadded}/*.* $traj_path/.
+  ((currentNumberOfReplicas--))
 
-  cp ./common/*.rtf $traj_path/.  
-  cp ./common/*.psf $traj_path/.  
-  cp ./common/*.py $traj_path/.
-  cp ./common/*.prm $traj_path/.
-  cp ./common/*.sh $traj_path/.
-  cp ./common/*.xsc $traj_path/.
-
+  if [ $currentNumberOfReplicas -eq 0] 
+  then
+    ((directoryNumber++))
+    directoryNumberPadded=`printf %04d $directoryNumber`
+    currentNumberOfReplicas=$(cat inputs/${directoryNumberPadded}/numberOfReplicas.txt)
+  fi
+     
   # each node has 6 GPUs, so the following modulo makes sure each traj is assigned
   # is assigned a GPU # in the range 0-5.
   gpu_number=$(expr $traj_number % 6)
