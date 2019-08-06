c25432366a03f2726dda1336434be02474265ebd
Derek Shore <des2037@med.cornell.edu>
2019-08-06 11:11:24 -0400

    adds jobName variable

--- launch_swarm.sh
+++ launch_swarm.sh
@@ -13,6 +13,7 @@ swarm_number=0
 number_of_trajs_per_swarm=18
 first_subjob=0
 last_subjob=3
+jobName="your job name"
 
 # do not edit below this line
 
@@ -23,9 +24,9 @@ for (( subjob=$first_subjob; subjob<=$last_subjob; subjob++ ))
 do
   if [ $first_iteration -eq 0 ]
   then
-    job_scheduler_output="$(bsub -P BIP180 -W 2:00 -nnodes $number_of_nodes -J openMM_test_ensemble -alloc_flags gpumps ./submit_swarm_subjobs.sh $swarm_number $number_of_trajs_per_swarm $subjob)"
+    job_scheduler_output="$(bsub -P BIP180 -W 2:00 -nnodes $number_of_nodes -J $jobName -alloc_flags gpumps ./submit_swarm_subjobs.sh $swarm_number $number_of_trajs_per_swarm $subjob)"
   else
-    job_scheduler_output="$(bsub -P BIP180 -W 2:00 -nnodes $number_of_nodes -J openMM_test_ensemble -alloc_flags gpumps -w $job_scheduler_number ./submit_swarm_subjobs.sh $swarm_number $number_of_trajs_per_swarm $subjob)"
+    job_scheduler_output="$(bsub -P BIP180 -W 2:00 -nnodes $number_of_nodes -J $jobName -alloc_flags gpumps -w $job_scheduler_number ./submit_swarm_subjobs.sh $swarm_number $number_of_trajs_per_swarm $subjob)"
   fi
 
   job_scheduler_number=$(echo $job_scheduler_output | awk '{print $2}' | sed -e 's/<//' | sed -e 's/>//')
