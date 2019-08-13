41b4c20f8a77ded91dafefee1c80965d17f70eda
Derek Shore <des2037@med.cornell.edu>
2019-08-13 15:19:01 -0400

    more lsf to Slurm changes

--- submit_swarm_subjobs.sh
+++ submit_swarm_subjobs.sh
@@ -10,18 +10,15 @@ subjob_number=$3
 # do not edit below this line
 
 swarm_number_padded=`printf %04d $swarm_number`
+subjob_number_padded=`printf %04d $subjob_number`
 
 CWD=`pwd`
 swarm_path=$CWD/raw_swarms/swarm${swarm_number_padded}
 
-for ((traj_number=0; traj_number<$number_of_trajs_per_swarm; traj_number++))
-do
-  traj_number_padded=`printf %04d $traj_number`
-  traj_path=$swarm_path/swarm${swarm_number_padded}_traj$traj_number_padded
-  
-  erf_file=$(ls ${traj_path}/*.erf)
 
-  cd $traj_path
-  jsrun --smpiargs="none" --erf_input $erf_file ./run_python.sh $subjob_number &
-  cd $CWD
-done 
+traj_number_padded=`printf %04d $SLURM_ARRAY_TASK_ID`
+traj_path=$swarm_path/swarm${swarm_number_padded}_traj$traj_number_padded
+
+cd $traj_path
+
+./run_python $subjob_number > ./python_log.txt 2>&1 &
