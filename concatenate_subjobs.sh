6a4aca1c2d3c48d365e1a36fb6ed8472d7f7acd2
Derek Shore <des2037@med.cornell.edu>
2019-08-28 12:48:19 -0400

    converts catdcd scripts to use Slurm

--- concatenate_subjobs.sh
+++ concatenate_subjobs.sh
@@ -9,22 +9,20 @@
 swarm_number=$1
 number_of_trajs_per_swarm=$2
 catdcd=$3
-structure_file=$4 
 
 CWD=`pwd`
 swarm_number_padded=`printf %04d $swarm_number`
 swarm_path=$CWD/raw_swarms/swarm$swarm_number_padded
 swarm_concatenated_path=$CWD/swarms_concatenated_temp/$swarm_number_padded
-structure_file_path="$CWD/common/$structure_file"
 
 mkdir -p $swarm_concatenated_path
 
 for (( traj_number=0; traj_number<$number_of_trajs_per_swarm; traj_number++ ))
 do
   traj_number_padded=`printf %04d $traj_number`
-  traj_path=$swarm_path/swarm${swarm_number_padded}_traj$traj_number_padded
+  traj_path=$swarm_path/swarm${swarm_number_padded}_traj${traj_number_padded}
   cd $traj_path
-  $catdcd -o $swarm_concatenated_path/${traj_number_padded}.trr -otype trr -s $structure_file_path *.dcd
+  $catdcd -o $swarm_concatenated_path/swarm${swarm_number_padded}_traj${traj_number_padded}.dcd -otype dcd -s ${swarm_concatenated_path}/*.psf *.dcd
 done
 
 exit
--- launch_concatenate_subjobs.sh
+++ launch_concatenate_subjobs.sh
@@ -1,18 +1,18 @@
-#!/bin/bash
-## Begin LSF Directives
-#BSUB -P BIP180                     # project name
-#BSUB -W 0:30                       # hours:minutes, with 2 hour maximum
-#BSUB -nnodes 1                     # this should equal: number_of_trajs/6 
-#BSUB -J test_concatenate_subjobs   # job name
+#!/bin/bash -l
+#SBATCH -p edison                   # partition name
+#SBATCH -t 0-02:00:00               # days-hours:minutes:seconds
+#SBATCH -N1                         # number of nodes 
+#SBATCH -n1                         # number of processes
+#SBATCH --mem=40G                   # memory
+#SBATCH -J concatenate_subjobs      # job name
 
 swarm_number=0
-number_of_trajs_per_swarm=18
-catdcd="/gpfs/alpine/proj-shared/bip180/vmd/vmd_library/plugins/OPENPOWER/bin/catdcd5.1/catdcd"
-structure_file='dat_phase2b4.coor' # must be in ./common
+number_of_trajs_per_swarm=40
+catdcd="/home/des2037/vmd3/lib/vmd3/plugins/LINUXAMD64/bin/catdcd5.1/catdcd"
 
 # do not edit below this line
 
-jsrun -n1 -c1 ./concatenate_subjobs.sh $swarm_number $number_of_trajs_per_swarm $catdcd $structure_file > log_concatenate_subjobs.txt 
+./concatenate_subjobs.sh $swarm_number $number_of_trajs_per_swarm $catdcd > log_concatenate_subjobs.txt 
 
 exit
 
